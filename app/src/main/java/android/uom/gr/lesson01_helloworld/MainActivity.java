9fb90db314382d7f15d7124e6aa0722bf1d59ac0
Theodoros Chaikalis <theodoros.chaikalis@intrasoft-intl.com>
2020-10-11 13:22:22 +0300

    add image loader with picasso

--- app/src/main/java/android/uom/gr/lesson01_helloworld/MainActivity.java
+++ app/src/main/java/android/uom/gr/lesson01_helloworld/MainActivity.java
@@ -4,9 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final List<String> images = new ArrayList<>();
+    private static Random randomGen = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,13 +24,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = this.findViewById(R.id.button);
+        final ImageView imageView = this.findViewById(R.id.imageView2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_LONG)
+                Picasso picasso = Picasso.get();
+                picasso.setLoggingEnabled(true);
+                picasso.load(getRandomImage()).into(imageView);
+                Toast.makeText(MainActivity.this, "Loading image...", Toast.LENGTH_SHORT)
                         .show();
             }
         });
+    }
+
+
+    private String getRandomImage(){
+        images.add("http://i.imgur.com/DvpvklR.png");
+        images.add("https://via.placeholder.com/468x900?text=Android+Mobile+Developement");
+        images.add("https://images.fineartamerica.com/images-medium-large-5/bahamas-scene-vertical-bob-sample.jpg");
+        images.add("https://media.gettyimages.com/vectors/vertical-design-layout-template-polygonal-orange-and-yellow-sample-vector-id501195367");
+
+       return images.get(randomGen.nextInt(images.size()));
     }
 }
