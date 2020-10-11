09ec6b5e5c7c9ed3379ec6905bc808d26053dfb2
Theodoros Chaikalis <theodoros.chaikalis@intrasoft-intl.com>
2020-10-11 12:19:25 +0300

    add listener to button

--- app/src/main/java/android/uom/gr/lesson01_helloworld/MainActivity.java
+++ app/src/main/java/android/uom/gr/lesson01_helloworld/MainActivity.java
@@ -2,6 +2,9 @@ package android.uom.gr.lesson01_helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = this.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
